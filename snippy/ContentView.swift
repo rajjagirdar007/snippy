1589775e82930bada7abd853af07d01514202d7b
Raj Jagirdar <133992843+rajjagirdar007@users.noreply.github.com>
2025-01-17 21:20:43 -0500

    Initial Commit

---
+++ snippy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  snippy
+//
+//  Created by Raj Jagirdar on 1/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ snippy/snippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  snippyApp.swift
+//  snippy
+//
+//  Created by Raj Jagirdar on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct snippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
